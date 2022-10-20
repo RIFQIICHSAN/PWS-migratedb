8f997f448ec1e53c69c70a8df3f3b86c58ab5e01
RIFQIICHSAN <rifqiican872@gmail.com>
2022-10-20 09:12:49 +0700

    adds plugin maven

---
+++ src/main/java/pabdjaya/pabdjaya/PabdjayaApplication.java
@@ -0,0 +1,13 @@
+package pabdjaya.pabdjaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PabdjayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PabdjayaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pabdjaya/pabdjaya/PabdjayaApplicationTests.java
@@ -0,0 +1,13 @@
+package pabdjaya.pabdjaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PabdjayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
